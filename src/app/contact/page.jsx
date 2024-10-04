4a0c12aa83f6ab88ff0dd4c2dc125895ac087f97
Thayiya <aravindat23@gmail.com>
2024-10-04 22:00:00 +0530

    color change

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -19,14 +19,13 @@ const ContactUsForm = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // Handle form submission (e.g., send to an API or email)
     console.log('Form submitted:', formData);
   };
 
   return (
-    <section className="py-16 ">
+    <section className="py-16  ">
       <div className="container mx-auto px-6">
-        <h2 className="text-3xl font-bold text-center mb-8">Contact Us</h2>
+        <h2 className="text-3xl font-bold text-center mb-8 text-navcolor">Contact Us</h2>
         <div className="max-w-lg mx-auto bg-white p-8 rounded-lg shadow-lg">
           <form onSubmit={handleSubmit}>
             <div className="mb-4">
@@ -74,7 +73,7 @@ const ContactUsForm = () => {
             </div>
             <button
               type="submit"
-              className="w-full bg-indigo-600 text-white py-2 px-4 rounded-md shadow-sm hover:bg-indigo-800 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+              className="w-full bg-fottercolor text-white py-2 px-4 rounded-md shadow-sm hover:bg-indigo-800 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
             >
               Send Message
             </button>
