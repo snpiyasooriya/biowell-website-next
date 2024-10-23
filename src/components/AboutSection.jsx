68fc661ee9277a1265b07fc5a9b020cd0eeab65d
Sayuru Piyasooriya <sayuru@sekahost.co.uk>
2024-10-23 18:24:29 +0530

    refactor(about): mobile

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -5,9 +5,9 @@ import logo1 from '../public/img/logo.svg'
 
 const AboutSection = () => {
   return (
-    <section className="relative min-h-[300px] sm:min-h-[500px] w-full mt-8 sm:mt-16 bg-transparent sm:bg-[#ADC4FF]" aria-label="About Biowell Pharmaceutical">
+    <section className="relative  w-full mt-10 sm:mt-16 bg-transparent md:bg-[#ADC4FF]" aria-label="About Biowell Pharmaceutical">
       {/* Background Image - Hidden on mobile */}
-      <div className="absolute inset-0 hidden sm:block">
+      <div className="absolute inset-0 hidden md:block">
         <div className="relative w-full h-full">
           <Image
             src={group}
@@ -22,21 +22,19 @@ const AboutSection = () => {
       </div>
 
       {/* Overlaying Content */}
-      <div className="relative z-10 flex flex-col items-center justify-center h-full text-center px-4 sm:px-8 py-10 sm:py-16 space-y-8 sm:space-y-8">
+      <div className="relative z-10 flex flex-col items-center justify-center h-full text-center mx-auto max-w-[288px] max-h-[201px]">
         {/* Logo */}
-        <h1 className="text-3xl sm:text-4xl md:text-6xl font-bold text-black">
-          <Image 
-            src={logo1} 
-            alt="Biowell Pharmaceutical Logo" 
+          <Image
+            src={logo1}
+            alt="Biowell Pharmaceutical Logo"
             className="w-[200px] h-[74px] sm:w-[340px] sm:h-[126px]"
             width={340}
             height={126}
           />
-        </h1>
 
         {/* Description */}
-        <div className='font-semibold opacity-80'>
-          <p className="text-[12px] sm:text-[16px] md:text-[20px] text-[#333333] max-w-3xl">
+        <div className='font-medium pt-5'>
+          <p className="text-[12px] md:text-[20px] text-[#808080] leading-[18px]">
             Founded in [Year], Biowell pharmaceutical (Pvt) Ltd. has grown from a small startup to a global leader in pharmaceutical manufacturing and trading. Our journey is marked by innovation, quality, and dedication to improving lives.
           </p>
         </div>
